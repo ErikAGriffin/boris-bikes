93633a6c10d7ab55bb9f90c3b3db407a0d9c7253
Carrie Christenson <christenson.carrie@gmail.com>
2015-02-03 16:00:40 +0000

    added Docking Station

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,20 +3,18 @@ require 'bike'
 describe Bike do 
 
 	let(:bike) { Bike.new }
+	let(:broken_bike) {Bike.new.break!}
 	
 	it 'should not be broken after we create it' do
 		expect(bike).not_to be_broken
 	end
 
 	it 'should be able to break' do
-		bike.break!
-		expect(bike).to be_broken
+		expect(bike.break!).to be_broken
 	end
 
 	it 'should be able to be fixed' do
-		bike.break!
-		bike.fix!
-		expect(bike).not_to be_broken
+		expect(broken_bike.fix!).not_to be_broken
 	end
 
 end
