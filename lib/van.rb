39af984fe5bc975542563159e5b313dbebfd2242
Erik Griffin <griffin.erik@gmail.com>
2015-02-03 19:47:33 +0000

    Added van functionality

--- lib/van.rb
+++ lib/van.rb
@@ -2,4 +2,20 @@ require 'BikeContainer'
 
 class Van < BikeContainer
 
+  def initialize
+    super
+
+    @location = 'garage';
+
+  end
+
+
+  def location
+    @location
+  end
+
+  def travel_to(destination)
+    @location = destination
+  end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,15 +1,41 @@
 require 'Van'
 
-describe Van do 
+describe Van do
 
 	let(:brokenBike) {double :bike, broken?: true, fix!: :workingBike}
   let(:workingBike) {double :bike, broken?: false, break!: :brokenBike}
+  let(:dockingStation) {double :dockingStation}
+
   let(:van) {Van.new}
+
+
 
   it 'should hold bikes' do
   	expect(van.bikeCount).to eq(0)
   	van.dock(workingBike)
   	expect(van.bikeCount).to eq(1)
   end
-	
+
+  it 'should release bikes' do
+    van.dock(workingBike)
+    van.release(workingBike)
+    expect(van.bikeCount).to eq(0)
+  end
+
+  it 'starts at garage' do
+    expect(van.location).to eq('garage')
+  end
+
+  it 'travel to dock station' do
+    van.travel_to(dockingStation)
+    expect(van.location).to eq(dockingStation)
+  end
+
+  it 'travel to garage' do
+    van.travel_to(dockingStation)
+    van.travel_to('garage')
+    expect(van.location).to eq('garage')
+  end
+
+
 end
